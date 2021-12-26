ba2e86960220a5d19739e0e11a1a77988d9cba43
rieselgold <github@rieselgold.com>
2021-12-27 00:46:20 +0100

    First floor

--- tiles/animated-cobraBlue-5.tsx
+++ tiles/animated-cobraBlue-5.tsx
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="animated-cobraBlue-5" tilewidth="32" tileheight="32" tilecount="100" columns="5">
+<tileset version="1.5" tiledversion="1.7.2" name="animated-cobraBlue-5" tilewidth="32" tileheight="32" tilecount="200" columns="10">
  <properties>
   <property name="tilesetCopyright" value="&quot;King Cobra Blue&quot; by AntumDeluge is licensed unter CC-BY 4.0: https://opengameart.org/content/king-cobra. Animated by Thoddi"/>
  </properties>
- <image source="animated-cobraBlue-5.png" width="160" height="640"/>
+ <image source="animated-cobraBlue-5.png" width="320" height="640"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="300"/>
@@ -122,6 +122,126 @@
    <frame tileid="89" duration="300"/>
    <frame tileid="94" duration="300"/>
    <frame tileid="99" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="400"/>
+   <frame tileid="15" duration="400"/>
+   <frame tileid="25" duration="400"/>
+   <frame tileid="35" duration="400"/>
+   <frame tileid="45" duration="400"/>
+   <frame tileid="55" duration="400"/>
+   <frame tileid="65" duration="400"/>
+   <frame tileid="75" duration="400"/>
+   <frame tileid="85" duration="400"/>
+   <frame tileid="95" duration="400"/>
+   <frame tileid="105" duration="400"/>
+   <frame tileid="115" duration="400"/>
+   <frame tileid="125" duration="400"/>
+   <frame tileid="135" duration="400"/>
+   <frame tileid="145" duration="400"/>
+   <frame tileid="155" duration="400"/>
+   <frame tileid="165" duration="400"/>
+   <frame tileid="175" duration="400"/>
+   <frame tileid="185" duration="400"/>
+   <frame tileid="195" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="400"/>
+   <frame tileid="16" duration="400"/>
+   <frame tileid="26" duration="400"/>
+   <frame tileid="36" duration="400"/>
+   <frame tileid="46" duration="400"/>
+   <frame tileid="56" duration="400"/>
+   <frame tileid="66" duration="400"/>
+   <frame tileid="76" duration="400"/>
+   <frame tileid="86" duration="400"/>
+   <frame tileid="96" duration="400"/>
+   <frame tileid="106" duration="400"/>
+   <frame tileid="116" duration="400"/>
+   <frame tileid="126" duration="400"/>
+   <frame tileid="136" duration="400"/>
+   <frame tileid="146" duration="400"/>
+   <frame tileid="156" duration="400"/>
+   <frame tileid="166" duration="400"/>
+   <frame tileid="176" duration="400"/>
+   <frame tileid="186" duration="400"/>
+   <frame tileid="196" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="400"/>
+   <frame tileid="17" duration="400"/>
+   <frame tileid="27" duration="400"/>
+   <frame tileid="37" duration="400"/>
+   <frame tileid="47" duration="400"/>
+   <frame tileid="57" duration="400"/>
+   <frame tileid="67" duration="400"/>
+   <frame tileid="77" duration="400"/>
+   <frame tileid="87" duration="400"/>
+   <frame tileid="97" duration="400"/>
+   <frame tileid="107" duration="400"/>
+   <frame tileid="117" duration="400"/>
+   <frame tileid="127" duration="400"/>
+   <frame tileid="137" duration="400"/>
+   <frame tileid="147" duration="400"/>
+   <frame tileid="157" duration="400"/>
+   <frame tileid="167" duration="400"/>
+   <frame tileid="177" duration="400"/>
+   <frame tileid="187" duration="400"/>
+   <frame tileid="197" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="400"/>
+   <frame tileid="18" duration="400"/>
+   <frame tileid="28" duration="400"/>
+   <frame tileid="38" duration="400"/>
+   <frame tileid="48" duration="400"/>
+   <frame tileid="58" duration="400"/>
+   <frame tileid="68" duration="400"/>
+   <frame tileid="78" duration="400"/>
+   <frame tileid="88" duration="400"/>
+   <frame tileid="98" duration="400"/>
+   <frame tileid="108" duration="400"/>
+   <frame tileid="118" duration="400"/>
+   <frame tileid="128" duration="400"/>
+   <frame tileid="138" duration="400"/>
+   <frame tileid="148" duration="400"/>
+   <frame tileid="158" duration="400"/>
+   <frame tileid="168" duration="400"/>
+   <frame tileid="178" duration="400"/>
+   <frame tileid="188" duration="400"/>
+   <frame tileid="198" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="400"/>
+   <frame tileid="19" duration="400"/>
+   <frame tileid="29" duration="400"/>
+   <frame tileid="39" duration="400"/>
+   <frame tileid="49" duration="400"/>
+   <frame tileid="59" duration="400"/>
+   <frame tileid="69" duration="400"/>
+   <frame tileid="79" duration="400"/>
+   <frame tileid="89" duration="400"/>
+   <frame tileid="99" duration="400"/>
+   <frame tileid="109" duration="400"/>
+   <frame tileid="119" duration="400"/>
+   <frame tileid="129" duration="400"/>
+   <frame tileid="139" duration="400"/>
+   <frame tileid="149" duration="400"/>
+   <frame tileid="159" duration="400"/>
+   <frame tileid="169" duration="400"/>
+   <frame tileid="179" duration="400"/>
+   <frame tileid="189" duration="400"/>
+   <frame tileid="199" duration="400"/>
   </animation>
  </tile>
 </tileset>
