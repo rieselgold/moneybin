49da3cf8d50023af05f4ba2039c9f9d6fe043a52
rieselgold <github@rieselgold.com>
2021-12-26 03:24:11 +0100

    Phonebooth

---
+++ tiles/phonebooth.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="phonebooth" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <properties>
+  <property name="tilesetCopyright" value="&quot;Phonebooth&quot; by flederrattie is licensed under CC BY"/>
+ </properties>
+ <image source="phonebooth.png" width="96" height="128"/>
+</tileset>
